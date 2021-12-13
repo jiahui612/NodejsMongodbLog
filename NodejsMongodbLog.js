bcd057e13c823b6109b4d1f6200d73942761fe6c
jiahui <1474349359@qq.com>
2021-12-12 17:43:32 -0800

    ejs  not basic ok

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,6 +1,8 @@
+const ejs=require('ejs')
 const express=require("express")
 const app=express()
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb://172.21.2.236:27017/190110910612');
 const schema={
     name:String,
@@ -19,5 +21,8 @@ app.get("/input",(req,res)=>{
     //const kitty = new mydata({ name: req.query.first,health:req.query.second });
     const kitty = new mydata({ name:'aaaa'});
     kitty.save()
+    ejs.renderFile("result.html",{returnVal:"success"},(err,str)=>{
+        res.send(str)
+    })
  })
 app.listen(10612)
